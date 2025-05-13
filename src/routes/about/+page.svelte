6ec319ac2f4876b01fb19ff50cbc66d640cbf157
Andrew Mueller <amueller@caltech.edu>
2025-05-13 10:16:45 -0700

    ironfell page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<div class="prose max-w-none w-full mx-auto mt-6 lg:pr-4">
+	<div class="border-y lg:border-t-0 border-gray-300 p-6 h-37 relative">
+		<h2 class="font-serif font-medium lg:h-4 justify-end flex-col block mt-16">
+			About me
+		</h2>
+
+		<div
+			class="absolute size-2 z-10 rounded-[1px] rotate-45 border border-gray-300 dark:border-blue-300/25 bg-gray-50 dark:bg-black left-[-4.5px] bottom-[-4.5px]"
+		></div>
+	</div>
+
+	<div class="font-serif font-medium p-7 px-8">
+		
+
+
+
+
+        <h3>About this site</h3>
+        
+	</div>
+</div>
+
+<style>
+	.prose {
+		overflow-y: unset;
+	}
+</style>
